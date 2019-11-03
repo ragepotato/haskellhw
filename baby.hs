936a73f65ecf9f5daed8bc6d2db595cccf9e69e5
Stephen Tayag <stephen.tayag@gmail.com>
2019-11-03 13:01:46 -0800

    Types and Typeclasses

--- baby.hs
+++ baby.hs
@@ -7,4 +7,13 @@ doubleSmallNumber' x = (doubleSmallNumber x) + 1
 conanO'Brien = "It's a-me, Conan O'Brien!"
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x] 
 length' xs = sum [1 | _ <- xs]
+removeNonUppercase :: String -> String
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']] 
+addThree :: Int -> Int -> Int -> Int  
+addThree x y z = x + y + z  
+factorial :: Integer -> Integer  
+factorial n = product [1..n] 
+circumference :: Float -> Float  
+circumference r = 2 * pi * r  
+circumference' :: Double -> Double  
+circumference' r = 2 * pi * r  
