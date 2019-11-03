115dd65f8a4208a1782ea5b1a44680d448593041
Stephen Tayag <stephen.tayag@gmail.com>
2019-11-02 22:56:39 -0700

    I'm a list comprehension

--- baby.hs
+++ baby.hs
@@ -5,3 +5,6 @@ doubleSmallNumber x = if x > 100
                         else x*2
 doubleSmallNumber' x = (doubleSmallNumber x) + 1 
 conanO'Brien = "It's a-me, Conan O'Brien!"
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x] 
+length' xs = sum [1 | _ <- xs]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']] 
