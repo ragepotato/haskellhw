bcd9c6c6b9c9cc688566ae22142da9f513f06270
Stephen Tayag <stephen.tayag@gmail.com>
2019-11-03 17:03:53 -0800

    Syntax in Functions 2

--- baby.hs
+++ baby.hs
@@ -42,3 +42,26 @@ second :: (a, b, c) -> b
 second (_, y, _) = y  
 third :: (a, b, c) -> c  
 third (_, _, z) = z  
+head' :: [a] -> a  
+head' [] = error "Can't call head on an empty list, dummy!"  
+head' (x:_) = x  
+tell :: (Show a) => [a] -> String  
+tell [] = "The list is empty"  
+tell (x:[]) = "The list has one element: " ++ show x  
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y  
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y  
+length'' :: (Num b) => [a] -> b  
+length'' [] = 0  
+length'' (_:xs) = 1 + length' xs  
+sum' :: (Num a) => [a] -> a  
+sum' [] = 0  
+sum' (x:xs) = x + sum' xs  
+capital :: String -> String  
+capital "" = "Empty string, whoops!"  
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x] 
+bmiTell :: (RealFloat a) => a -> String  
+bmiTell bmi  
+    | bmi <= 18.5 = "You're underweight, you emo, you!"  
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | otherwise   = "You're a whale, congratulations!"
