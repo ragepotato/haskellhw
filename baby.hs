9fbac43a18fa5616a375233d64de4bbb78fc022b
Stephen Tayag <stephen.tayag@gmail.com>
2019-11-03 18:07:40 -0800

    Syntax in Functions 4

--- baby.hs
+++ baby.hs
@@ -61,10 +61,14 @@ capital "" = "Empty string, whoops!"
 capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x] 
 bmiTell :: (RealFloat a) => a -> a -> String  
 bmiTell weight height  
-    | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"  
-    | weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
-    | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"  
-    | otherwise                 = "You're a whale, congratulations!"
+    | bmi <= skinny = "You're underweight, you emo, you!"  
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"  
+    | otherwise     = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2  
+          skinny = 18.5  
+          normal = 25.0  
+          fat = 30.0  
 max' :: (Ord a) => a -> a -> a  
 max' a b   
         | a > b     = a  
@@ -74,3 +78,15 @@ a `myCompare` b
         | a > b     = GT  
         | a == b    = EQ  
         | otherwise = LT 
+initials :: String -> String -> String  
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."  
+    where (f:_) = firstname  
+          (l:_) = lastname  
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]  
+calcBmis xs = [bmi w h | (w, h) <- xs]  
+    where bmi weight height = weight / height ^ 2
+cylinder :: (RealFloat a) => a -> a -> a  
+cylinder r h = 
+    let sideArea = 2 * pi * r * h  
+        topArea = pi * r ^2  
+    in  sideArea + 2 * topArea 
