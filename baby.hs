c8a616fdd9dbc2e967cc5e4719cff8f1b5ee3b17
Stephen Tayag <stephen.tayag@gmail.com>
2019-11-05 14:08:11 -0800

    Higher Order Functions

--- baby.hs
+++ baby.hs
@@ -141,3 +141,23 @@ quicksort (x:xs) =
     let smallerSorted = quicksort [a | a <- xs, a <= x]  
         biggerSorted = quicksort [a | a <- xs, a > x]  
     in  smallerSorted ++ [x] ++ biggerSorted 
+multThree :: (Num a) => a -> a -> a -> a  
+multThree x y z = x * y * z  
+compareWithHundred :: (Num a, Ord a) => a -> Ordering  
+compareWithHundred = compare 100 
+divideByTen :: (Floating a) => a -> a  
+divideByTen = (/10) 
+isUpperAlphanum :: Char -> Bool  
+isUpperAlphanum = (`elem` ['A'..'Z'])
+applyTwice :: (a -> a) -> a -> a  
+applyTwice f x = f (f x) 
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]  
+zipWith' _ [] _ = []  
+zipWith' _ _ [] = []  
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+flip' :: (a -> b -> c) -> b -> a -> c  
+flip' f y x = f x y  
+map :: (a -> b) -> [a] -> [b]  
+map _ [] = []  
+map f (x:xs) = f x : map f xs  
+ 
