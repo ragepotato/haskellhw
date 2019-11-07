a448191d2ab1e74390cc39b6559cadc648202438
Stephen Tayag <stephen.tayag@gmail.com>
2019-11-07 09:25:19 -0800

    Higher Order Functions 2

--- baby.hs
+++ baby.hs
@@ -156,8 +156,47 @@ zipWith' _ [] _ = []
 zipWith' _ _ [] = []  
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 flip' :: (a -> b -> c) -> b -> a -> c  
-flip' f y x = f x y  
-map :: (a -> b) -> [a] -> [b]  
-map _ [] = []  
-map f (x:xs) = f x : map f xs  
- 
+flip' f = \x y -> f y x   
+quicksort' :: (Ord a) => [a] -> [a]    
+quicksort' [] = []    
+quicksort' (x:xs) =     
+    let smallerSorted = quicksort' (filter (<=x) xs)  
+        biggerSorted = quicksort' (filter (>x) xs)   
+    in  smallerSorted ++ [x] ++ biggerSorted  
+largestDivisible :: (Integral a) => a  
+largestDivisible = head (filter p [100000,99999..])  
+    where p x = x `mod` 3829 == 0  
+chain :: (Integral a) => a -> [a]  
+chain 1 = [1]  
+chain n  
+    | even n =  n:chain (n `div` 2)  
+    | odd n  =  n:chain (n*3 + 1)  
+numLongChains :: Int  
+numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+addThree' :: (Num a) => a -> a -> a -> a  
+addThree' = \x -> \y -> \z -> x + y + z   
+sum''' :: (Num a) => [a] -> a     
+sum''' xs = foldl (+) 0 xs  
+elem'' :: (Eq a) => a -> [a] -> Bool  
+elem'' y ys = foldl (\acc x -> if x == y then True else acc) False ys  
+maximums'' :: (Ord a) => [a] -> a  
+maximums'' = foldr1 (\x acc -> if x > acc then x else acc)  
+reverse'' :: [a] -> [a]  
+reverse'' = foldl (\acc x -> x : acc) []  
+product'' :: (Num a) => [a] -> a  
+product'' = foldr1 (*)  
+filter'' :: (a -> Bool) -> [a] -> [a]  
+filter'' p = foldr (\x acc -> if p x then x : acc else acc) []  
+head''' :: [a] -> a  
+head''' = foldr1 (\x _ -> x)  
+last'' :: [a] -> a  
+last'' = foldl1 (\_ x -> x)
+sqrtSums :: Int  
+sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
+($) :: (a -> b) -> a -> b  
+f $ x = f x  
+(.) :: (b -> c) -> (a -> b) -> a -> c  
+f . g = \x -> f (g x)
+fn x = ceiling (negate (tan (cos (max 50 x))))
+oddSquareSum :: Integer  
+oddSquareSum = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))  
