0a3ffb1f82ab70f3572b22b3c1927981184a9820
Stephen Tayag <stephen.tayag@gmail.com>
2019-11-08 15:23:59 -0800

    Modules

--- baby.hs
+++ baby.hs
@@ -1,3 +1,16 @@
+import Data.List 
+import Prelude hiding ((.))
+import Data.List hiding (nub)
+--import qualified Data.Map 
+import qualified Data.List as L
+import qualified Data.Map as Map  
+import qualified Data.Set as Set
+import qualified Data.Char as C
+import Data.Function (on)
+import Geometry
+import qualified Geometry.Sphere as Sphere  
+import qualified Geometry.Cuboid as Cuboid  
+import qualified Geometry.Cube as Cube 
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
@@ -193,10 +206,43 @@ last'' :: [a] -> a
 last'' = foldl1 (\_ x -> x)
 sqrtSums :: Int  
 sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
-($) :: (a -> b) -> a -> b  
-f $ x = f x  
-(.) :: (b -> c) -> (a -> b) -> a -> c  
+--($) :: (a -> b) -> a -> b  
+--f $ x = f x  
+--(.) :: (b -> c) -> (a -> b) -> a -> c  
 f . g = \x -> f (g x)
 fn x = ceiling (negate (tan (cos (max 50 x))))
 oddSquareSum :: Integer  
 oddSquareSum = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))  
+numUniques :: (Eq a) => [a] -> Int  
+numUniques = length . nub 
+search :: (Eq a) => [a] -> [a] -> Bool  
+search needle haystack =   
+    let nlen = length needle  
+    in  foldl (\acc x -> if take nlen x == needle then True else acc) False (tails haystack)
+encode :: Int -> String -> String  
+encode shift msg = 
+    let ords = L.map C.ord msg  
+        shifted = map (+ shift) ords  
+    in  L.map C.chr shifted  
+decode :: Int -> String -> String  
+decode shift msg = encode (negate shift) msg 
+phoneBook =   
+    [("betty","555-2938")  
+    ,("betty","342-2492")  
+    ,("bonnie","452-2928")  
+    ,("patsy","493-2928")  
+    ,("patsy","943-2929")  
+    ,("patsy","827-9162")  
+    ,("lucille","205-2928")  
+    ,("wendy","939-8282")  
+    ,("penny","853-2492")  
+    ,("penny","555-2111")  
+    ]  
+findKey :: (Eq k) => k -> [(k,v)] -> Maybe v  
+findKey key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing  
+fromList' :: (Ord k) => [(k,v)] -> Map.Map k v  
+fromList' = foldr (\(k,v) acc -> Map.insert k v acc) Map.empty  
+phoneBookToMap :: (Ord k) => [(k, a)] -> Map.Map k [a]  
+phoneBookToMap xs = Map.fromListWith (++) $ map (\(k,v) -> (k,[v])) xs  
+text1 = "I just had an anime dream. Anime... Reality... Are they so different?"  
+text2 = "The old man left his garbage can out and now his trash is all over my lawn!" 
