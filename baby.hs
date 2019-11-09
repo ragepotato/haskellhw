0e6d24f378fa516d8c8e5cc154964f72a13c18fd
Stephen Tayag <stephen.tayag@gmail.com>
2019-11-09 00:11:51 -0800

    Making Our Own Types and Typeclasses 2

--- baby.hs
+++ baby.hs
@@ -1,5 +1,5 @@
 import Data.List 
-import Prelude hiding ((.), Maybe, Just, Nothing)
+import Prelude hiding ((.), Maybe, Just, Nothing, (/=))
 import Data.List hiding (nub)
 --import qualified Data.Map 
 import qualified Data.List as L
@@ -279,3 +279,76 @@ vectMult :: (Num t) => Vector t -> t -> Vector t
   
 scalarMult :: (Num t) => Vector t -> Vector t -> t  
 (Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n  
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday   
+           deriving (Eq, Ord, Show, Read, Bounded, Enum)   
+infixr 5 :-:  
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord) 
+infixr 5  .++  
+(.++) :: List a -> List a -> List a   
+Empty .++ ys = ys  
+(x :-: xs) .++ ys = x :-: (xs .++ ys) 
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)  
+singleton :: a -> Tree a  
+singleton x = Node x EmptyTree EmptyTree  
+treeInsert :: (Ord a) => a -> Tree a -> Tree a  
+treeInsert x EmptyTree = singleton x  
+treeInsert x (Node a left right)   
+    | x == a = Node x left right  
+    | x < a  = Node a (treeInsert x left) right  
+    | x > a  = Node a left (treeInsert x right)
+treeElem :: (Ord a) => a -> Tree a -> Bool  
+treeElem x EmptyTree = False  
+treeElem x (Node a left right)  
+    | x == a = True  
+    | x < a  = treeElem x left  
+    | x > a  = treeElem x right 
+{-# LANGUAGE DefaultSignatures, DeriveAnyClass #-}  
+data TrafficLight = Red | Yellow | Green  
+instance Eq TrafficLight where  
+    Red == Red = True  
+    Green == Green = True  
+    Yellow == Yellow = True  
+    _ == _ = False
+instance Show TrafficLight where  
+    show Red = "Red light"  
+    show Yellow = "Yellow light"  
+    show Green = "Green light"  
+instance (Eq m) => Eq (Maybe m) where  
+    Just x == Just y = x == y  
+    Nothing == Nothing = True  
+    _ == _ = False  
+class YesNo a where  
+    yesno :: a -> Bool 
+instance YesNo Int where  
+    yesno 0 = False  
+    yesno _ = True 
+instance YesNo [a] where  
+    yesno [] = False  
+    yesno _ = True 
+instance YesNo Bool where  
+    yesno = id 
+instance YesNo (Maybe a) where  
+    yesno (Just _) = True  
+    yesno Nothing = False 
+instance YesNo (Tree a) where  
+    yesno EmptyTree = False  
+    yesno _ = True  
+instance YesNo TrafficLight where  
+    yesno Red = False  
+    yesno _ = True  
+yesnoIf :: (YesNo y) => y -> a -> a -> a  
+yesnoIf yesnoVal yesResult noResult = if yesno yesnoVal then yesResult else noResult
+instance Functor Maybe where  
+    fmap f (Just x) = Just (f x)  
+    fmap f Nothing = Nothing
+instance Functor Tree where  
+    fmap f EmptyTree = EmptyTree  
+    fmap f (Node x leftsub rightsub) = Node (f x) (fmap f leftsub) (fmap f rightsub)    
+data Frank a b  = Frank {frankField :: b a} deriving (Show)
+class Tofu t where  
+    tofu :: j a -> t a j  
+instance Tofu Frank where  
+    tofu x = Frank x  
+data Barry t k p = Barry { yabba :: p, dabba :: t k }
+instance Functor (Barry a b) where  
+    fmap f (Barry {yabba = x, dabba = y}) = Barry {yabba = f x, dabba = y}    
