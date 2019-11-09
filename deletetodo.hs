24b5bd31e28a7730c4de72e407fcd7e0f43afcb0
Stephen Tayag <stephen.tayag@gmail.com>
2019-11-09 14:21:06 -0800

    Input and Output 5

--- deletetodo.hs
+++ deletetodo.hs
@@ -1,7 +1,7 @@
 import System.IO  
 import System.Directory  
 import Data.List  
-  
+ 
 main = do        
     handle <- openFile "todo.txt" ReadMode  
     (tempName, tempHandle) <- openTempFile "." "temp"  
@@ -19,3 +19,4 @@ main = do
     hClose tempHandle  
     removeFile "todo.txt"  
     renameFile tempName "todo.txt"  
+
