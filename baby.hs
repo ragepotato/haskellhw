ed0e8ff6421a57a7774d425ec299d3036bfd232d
Stephen Tayag <stephen.tayag@gmail.com>
2019-11-09 00:35:13 -0800

    Input and Output

--- baby.hs
+++ baby.hs
@@ -352,3 +352,4 @@ instance Tofu Frank where
 data Barry t k p = Barry { yabba :: p, dabba :: t k }
 instance Functor (Barry a b) where  
     fmap f (Barry {yabba = x, dabba = y}) = Barry {yabba = f x, dabba = y}    
+ 
