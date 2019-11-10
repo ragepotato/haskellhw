82cf5fdd30ad74a651f9ab180955ad681ec17492
Stephen Tayag <stephen.tayag@gmail.com>
2019-11-10 15:48:59 -0800

    For a Few Monads More

---
+++ monads.hs
@@ -0,0 +1,38 @@
+
+import Data.Monoid  
+import Control.Monad.Writer  
+isBigGang :: Int -> (Bool, String)  
+isBigGang x = (x > 9, "Compared gang size to 9.")  
+applyLog :: (Monoid m) => (a,m) -> (a -> (b,m)) -> (b,m)  
+applyLog (x,log) f = let (y,newLog) = f x in (y,log `mappend` newLog)  
+type Food = String  
+type Price = Sum Int  
+  
+addDrink :: Food -> (Food,Price)  
+addDrink "beans" = ("milk", Sum 25)  
+addDrink "jerky" = ("whiskey", Sum 99)  
+addDrink _ = ("beer", Sum 30)  
+newtype Writer w a = Writer { runWriter :: (a, w) }  
+gcd' :: Int -> Int -> Control.Monad.Writer.Writer [String] Int  
+gcd' a b  
+    | b == 0 = do  
+        tell ["Finished with " ++ show a]  
+        return a  
+    | otherwise = do  
+        tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]  
+        gcd' b (a `mod` b)  
+gcdReverse :: Int -> Int -> Control.Monad.Writer.Writer [String] Int  
+gcdReverse a b  
+    | b == 0 = do  
+        tell ["Finished with " ++ show a]  
+        return a  
+    | otherwise = do  
+        result <- gcdReverse b (a `mod` b)  
+        tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]  
+        return result  
+newtype DiffList a = DiffList { getDiffList :: [a] -> [a] }  
+toDiffList :: [a] -> DiffList a  
+toDiffList xs = DiffList (xs++)  
+  
+fromDiffList :: DiffList a -> [a]  
+fromDiffList (DiffList f) = f []  
