ebc6feb7ba76ce8cc08eda2077b5089f793ed5dd
Stephen Tayag <stephen.tayag@gmail.com>
2019-11-10 13:55:20 -0800

    A Fistful of Monads

--- fmapping_io.hs
+++ fmapping_io.hs
@@ -49,3 +49,37 @@ testTree = Node 5
                 (Node 8 Empty Empty)  
                 (Node 10 Empty Empty)  
             )  
+applyMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b  
+applyMaybe Nothing f  = Nothing  
+applyMaybe (Just x) f = f x  
+type Birds = Int  
+type Pole = (Birds,Birds)  
+landLeft :: Birds -> Pole -> Maybe Pole  
+landLeft n (left,right)  
+    | abs ((left + n) - right) < 4 = Just (left + n, right)  
+    | otherwise                    = Nothing  
+
+landRight :: Birds -> Pole -> Maybe Pole  
+landRight n (left,right)  
+    | abs (left - (right + n)) < 4 = Just (left, right + n)  
+    | otherwise                    = Nothing  
+x -: f = f x  
+banana :: Pole -> Maybe Pole  
+banana _ = Nothing 
+routine :: Maybe Pole  
+routine = case landLeft 1 (0,0) of  
+    Nothing -> Nothing  
+    Just pole1 -> case landRight 4 pole1 of   
+        Nothing -> Nothing  
+        Just pole2 -> case landLeft 2 pole2 of  
+            Nothing -> Nothing  
+            Just pole3 -> landLeft 1 pole3  
+foo :: Maybe String  
+foo = do  
+    x <- Just 3  
+    y <- Just "!"  
+    Just (show x ++ y)  
+marySue :: Maybe Bool  
+marySue = do   
+    x <- Just 9  
+    Just (x > 8) 
