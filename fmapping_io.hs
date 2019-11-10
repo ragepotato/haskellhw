01d3e3da2bb038898c62dcac4cdb5b607d3258df
Stephen Tayag <stephen.tayag@gmail.com>
2019-11-10 02:19:43 -0800

    Functors, Applicative Functors and Monoids

--- fmapping_io.hs
+++ fmapping_io.hs
@@ -1,8 +1,9 @@
 
 import Data.Char  
 import Data.List  
-import Prelude hiding (sequenceA)
-  
+import Prelude hiding (sequenceA, Monoid)
+import Data.Monoid
+import qualified Data.Foldable as F  
 main = do line <- fmap (intersperse '-' . reverse . map toUpper) getLine  
           putStrLn line  
 
@@ -16,3 +17,35 @@ liftA2 f a b = f <$> a <*> b
 
 sequenceA :: (Applicative f) => [f a] -> f [a]  
 sequenceA = foldr (liftA2 (:)) (pure [])  
+data ZipList a = ZipList { getZipList :: [a] }  
+data Profession = Fighter | Archer | Accountant  
+data Race = Human | Elf | Orc | Goblin  
+data PlayerCharacter = PlayerCharacter Race Profession  
+newtype CharList = CharList { getCharList :: [Char] } deriving (Eq, Show)
+newtype Pair b a = Pair { getPair :: (a,b) }
+instance Functor (Pair c) where  
+    fmap f (Pair (x,y)) = Pair (f x, y)  
+newtype CoolBool = CoolBool { getCoolBool :: Bool } 
+helloMe :: CoolBool -> String  
+helloMe (CoolBool _) = "hello"  
+type IntList = [Int]  
+lengthCompare :: String -> String -> Ordering  
+lengthCompare x y = (length x `compare` length y) `mappend`  
+                    (vowels x `compare` vowels y) `mappend`  
+                    (x `compare` y)  
+    where vowels = length . filter (`elem` "aeiou")
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)  
+instance F.Foldable Tree where  
+    foldMap f Empty = mempty  
+    foldMap f (Node x l r) = F.foldMap f l `mappend`  
+                             f x           `mappend`  
+                             F.foldMap f r  
+testTree = Node 5  
+            (Node 3  
+                (Node 1 Empty Empty)  
+                (Node 6 Empty Empty)  
+            )  
+            (Node 9  
+                (Node 8 Empty Empty)  
+                (Node 10 Empty Empty)  
+            )  
