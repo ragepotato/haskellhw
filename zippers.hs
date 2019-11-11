d8b13628220ba3d79520e076391e823a752b4508
Stephen Tayag <stephen.tayag@gmail.com>
2019-11-10 19:15:10 -0800

    Zippers 2

--- zippers.hs
+++ zippers.hs
@@ -38,13 +38,15 @@ elemAt (R:ds) (Node _ _ r) = elemAt ds r
 elemAt [] (Node x _ _) = x  
 data Crumb a = LeftCrumb a (Tree a) | RightCrumb a (Tree a) deriving (Show)
 type Breadcrumbs a = [Crumb a]  
-goLeft :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)  
-goLeft (Node x l r, bs) = (l, LeftCrumb x r:bs)  
-goRight :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)  
-goRight (Node x l r, bs) = (r, RightCrumb x l:bs)  
+goLeft :: Zipper a -> Maybe (Zipper a)  
+goLeft (Node x l r, bs) = Just (l, LeftCrumb x r:bs)  
+goLeft (Empty, _) = Nothing  
+goRight :: Zipper a -> Maybe (Zipper a)  
+goRight (Node x l r, bs) = Just (r, RightCrumb x l:bs)  
+goRight (Empty, _) = Nothing  
 goUp :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)  
 goUp (t, LeftCrumb x r:bs) = (Node x t r, bs)  
-goUp (t, RightCrumb x l:bs) = (Node x l t, bs)  
+goUp (t, RightCrumb x l:bs) = (Node x l t, bs)   
 type Zipper a = (Tree a, Breadcrumbs a)  
 modify :: (a -> a) -> Zipper a -> Zipper a  
 modify f (Node x l r, bs) = (Node (f x) l r, bs)  
