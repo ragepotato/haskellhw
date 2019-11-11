5f8e6f84f0c56e5d12871634040de319ba297fbf
Stephen Tayag <stephen.tayag@gmail.com>
2019-11-10 17:15:17 -0800

    For a Few Monads More

--- monads.hs
+++ monads.hs
@@ -1,6 +1,8 @@
 
 import Data.Monoid  
 import Control.Monad.Writer  
+import Control.Monad.Instances
+import qualified Control.Monad.State (State)
 isBigGang :: Int -> (Bool, String)  
 isBigGang x = (x > 9, "Compared gang size to 9.")  
 applyLog :: (Monoid m) => (a,m) -> (a -> (b,m)) -> (b,m)  
@@ -13,14 +15,7 @@ addDrink "beans" = ("milk", Sum 25)
 addDrink "jerky" = ("whiskey", Sum 99)  
 addDrink _ = ("beer", Sum 30)  
 newtype Writer w a = Writer { runWriter :: (a, w) }  
-gcd' :: Int -> Int -> Control.Monad.Writer.Writer [String] Int  
-gcd' a b  
-    | b == 0 = do  
-        tell ["Finished with " ++ show a]  
-        return a  
-    | otherwise = do  
-        tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]  
-        gcd' b (a `mod` b)  
+
 gcdReverse :: Int -> Int -> Control.Monad.Writer.Writer [String] Int  
 gcdReverse a b  
     | b == 0 = do  
@@ -36,3 +31,44 @@ toDiffList xs = DiffList (xs++)
   
 fromDiffList :: DiffList a -> [a]  
 fromDiffList (DiffList f) = f []  
+finalCountDown :: Int -> Control.Monad.Writer.Writer [String] ()  
+finalCountDown 0 = do  
+    tell ["0"]  
+finalCountDown x = do  
+    finalCountDown (x-1)  
+    tell [show x]  
+addStuff :: Int -> Int  
+addStuff x = let  
+    a = (*2) x  
+    b = (+10) x  
+    in a+b  
+
+newtype State s a = State { runState :: s -> (a,s) }  
+type Stack = [Int]  
+  
+pop :: State Stack Int  
+pop = Main.State $ \(x:xs) -> (x,xs)  
+  
+push :: Int -> State Stack ()  
+push a = State $ \xs -> ((),a:xs)  
+stackStuff :: State Stack ()  
+stackStuff = do  
+    a <- pop  
+    if a == 5  
+        then push 5  
+        else do  
+            push 3  
+            push 8  
+moreStack :: State Stack ()  
+moreStack = do  
+    a <- stackManip  
+    if a == 100  
+        then stackStuff  
+        else return ()  
+
+stackyStack :: State Stack ()  
+stackyStack = do  
+    stackNow <- get  
+    if stackNow == [1,2,3]  
+        then put [8,3,1]  
+        else put [9,2,1]  
